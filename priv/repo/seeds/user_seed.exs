5be6edc986d174feb9f33262570a83dc0eaed404
ahmadsyahirmuhamad <ahmadsyahirmuhamad@gmail.com>
2018-02-19 23:35:22 +0800

    add seed data

--- priv/repo/seeds/user_seed.exs
+++ priv/repo/seeds/user_seed.exs
@@ -2,13 +2,23 @@ alias ChatServer.Repo
 alias ChatServer.Account.User
 
 # Create users
-user_params = %{
-  email: "user_1@example.com",
+user_one = %{
+  email: "john_doe@example.com",
   password: "password",
   password_confirmation: "password",
-  first_name: "user",
-  last_name: "example"
+  first_name: "john",
+  last_name: "doe"
 }
-changeset = User.changeset(:create, %User{}, user_params)
+changeset = User.changeset(:create, %User{}, user_one)
 Repo.insert(changeset)
 
+
+user_two = %{
+  email: "jane_doe@example.com",
+  password: "password",
+  password_confirmation: "password",
+  first_name: "jane",
+  last_name: "example"
+}
+changeset = User.changeset(:create, %User{}, user_two)
+Repo.insert(changeset)
